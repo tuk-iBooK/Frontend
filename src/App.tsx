30927ba9f73de55720ff1f0e97f01dd2c4d00c43
HaeunKim01 <tobebeo382@naver.com>
2024-02-07 17:30:13 +0900

    css 적용

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">
+        My TypeScript React Tailwind Project
+      </h1>
+      <p className="mt-2">Welcome to your app!</p>
+    </div>
+  );
+}
+
+export default App;
