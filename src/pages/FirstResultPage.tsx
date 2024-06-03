37b3fc158bde25a98f65a0311c40affbdd6a502b
HaeunKim01 <tobebeo382@naver.com>
2024-06-03 16:37:40 +0900

    [fix]이야기,이미지 두번씩 생성 오류 수정 완료

--- src/pages/FirstResultPage.tsx
+++ src/pages/FirstResultPage.tsx
@@ -1,14 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  addPage,
-  addUserChoice,
-  updatePage,
-  setImage,
-} from "../features/storySlice";
+import { addPage, addUserChoice, setImage } from "../features/storySlice";
 
 import axios, { AxiosRequestConfig } from "axios";
-import Loading from "../components/Loading";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 
@@ -27,21 +21,21 @@ const FirstResultPage: React.FC = () => {
 
   const [loading, setLoading] = useState(true);
   const [isFetching, setIsFetching] = useState(false); // 중복 호출을 방지하기 위한 상태
+  const [config, setConfig] = useState<AxiosRequestConfig | null>(null);
 
-  const fetchConfig = () => {
+  useEffect(() => {
     const token = localStorage.getItem("id");
-    console.log("현재 토큰 :", token);
     if (!token) {
       console.error("토큰이 없습니다.");
-      return null;
+      return;
     }
-    return {
+    setConfig({
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
-    };
-  };
+    });
+  }, []);
 
   //서버 데이터 파싱하는 함수
   const parseResponse = (responseText: string) => {
@@ -66,23 +60,11 @@ const FirstResultPage: React.FC = () => {
       )
       .map((line) => line.substring(line.indexOf(" ") + 1).trim()); // 접두사 제외 문장만
 
-    return { title: title || "", content, choices };
+    const pageId = pages.length + 1; // 페이지 ID를 순차적으로 증가시키도록 설정 (예시)
+    console.log("pageId", pageId);
+
+    return { pageId, title: title || "", content, choices };
   };
-
-  // useEffect(() => {
-  //   const config = fetchConfig();
-  //   if (config) {
-  //     fetchData(story, config);
-  //   }
-  // }, [story]);
-
-  // useEffect(() => {
-  //   console.log("fetchData 호출 전:", story);
-  //   const config = fetchConfig();
-  //   if (config) {
-  //     fetchData(story, config);
-  //   }
-  // }, []); // 의존성 배열을 빈 배열로 설정
 
   useEffect(() => {
     console.log("Pages 업데이트됨:", pages);
@@ -94,6 +76,7 @@ const FirstResultPage: React.FC = () => {
   };
 
   const fetchData = async (story: number, config: AxiosRequestConfig) => {
+    if (!config) return;
     setLoading(true);
     try {
       // API 요청 : 첫번째 이야기 생성
@@ -104,7 +87,6 @@ const FirstResultPage: React.FC = () => {
       );
       if (response.status === 200) {
         const parsedData = parseResponse(response.data.answer);
-
         dispatch(addPage({ ...parsedData }));
         console.log("Redux에 페이지 추가됨", parsedData);
 
@@ -115,6 +97,7 @@ const FirstResultPage: React.FC = () => {
           config
         );
         console.log("이야기 저장 성공! :", saveStoryResponse);
+        await createAndSaveImage(parsedData.pageId, parsedData.content);
       } else {
         console.error("API 요청 실패 :", response);
       }
@@ -126,60 +109,33 @@ const FirstResultPage: React.FC = () => {
   };
 
   useEffect(() => {
-    console.log("Component mounted, fetching data for story:", story);
-    if (!isFetching) {
-      const config = fetchConfig();
-      if (config) {
-        setIsFetching(true);
-        fetchData(story, config).finally(() => setIsFetching(false));
-      }
+    if (config && !isFetching) {
+      setIsFetching(true);
+      fetchData(story, config).finally(() => setIsFetching(false));
     }
-  }, [story]); // 스토리 ID가 변경되지 않는 경우, 이 useEffect는 실행되지 않습니다.
-
-  // useEffect(() => {
-  //   console.log("currentPage 변경됨, 이미지 생성 검토:", currentPage);
-  //   if (currentPage && !currentPage.imageUrl) {
-  //     createAndSaveImage(currentPage.pageId);
-  //   }
-  // }, [currentPage?.imageUrl, currentPage?.pageId]); // 의존성 배열 수정 (<< currentPage)
+  }, [story, config]);
 
   // useEffect(() => {
   //   if (currentPage && !currentPage.imageUrl && !isFetching) {
-  //     console.log(
-  //       "Creating and saving image for currentPage:",
-  //       currentPage.pageId
-  //     );
   //     setIsFetching(true);
   //     createAndSaveImage(currentPage.pageId).finally(() =>
   //       setIsFetching(false)
   //     );
   //   }
-  // }, [currentPage?.imageUrl, currentPage?.pageId, dispatch]);
+  // }, [currentPage?.imageUrl, currentPage?.pageId, config, dispatch]);
 
-  useEffect(() => {
-    if (currentPage && !currentPage.imageUrl && !isFetching) {
-      console.log(
-        "Creating and saving image for currentPage:",
-        currentPage.pageId
-      );
-      setIsFetching(true);
-      createAndSaveImage(currentPage.pageId).finally(() =>
-        setIsFetching(false)
-      );
-    }
-  }, [currentPage?.imageUrl, currentPage?.pageId, dispatch]);
-  useEffect(() => {
-    setLoading(false); // 이미지 로딩 완료 후 로딩 상태 업데이트
-  }, [currentPage?.imageUrl]);
+  // useEffect(() => {
+  //   setLoading(false); // 이미지 로딩 완료 후 로딩 상태 업데이트
+  // }, [currentPage?.imageUrl]);
 
-  const createAndSaveImage = async (pageId: number) => {
-    const config = fetchConfig();
+  const createAndSaveImage = async (pageId: number, content: string) => {
     if (!config) return;
     // 선택에 따른 이미지 생성 요청
     try {
       const imageResponse = await axios.post(
         "http://localhost:8000/api/story/register/chatgpt/image/",
-        { story_id: story, query: pages[pages.length - 1].content },
+        // { story_id: story, query: pages[pages.length].content },
+        { story_id: story, query: content },
         config
       );
       console.log("이야기 생성 성공 :", imageResponse);
@@ -187,9 +143,7 @@ const FirstResultPage: React.FC = () => {
       // 이미지 저장 요청
       if (imageResponse.status === 200 && imageResponse.data.image_url) {
         dispatch(setImage({ pageId, imageUrl: imageResponse.data.image_url }));
-
-        console.log("Redux 상태에 이미지 URL 설정");
-        console.log("url :", imageResponse.data.image_url);
+        console.log("리덕스에 저장된 url :", imageResponse.data.image_url);
 
         const saveImageResponse = await axios.post(
           "http://localhost:8000/api/story/save_image/",
@@ -211,12 +165,10 @@ const FirstResultPage: React.FC = () => {
 
   const handleChoice = useCallback(
     async (choice: string) => {
+      if (!config) return;
       dispatch(
         addUserChoice({ pageId: currentPage?.pageId || 0, userChoice: choice })
       );
-      const config = fetchConfig();
-      if (!config) return;
-
       setLoading(true);
       try {
         // 다음 API 요청: 이야기 생성
@@ -229,14 +181,11 @@ const FirstResultPage: React.FC = () => {
 
         if (updateResponse.status === 200 && updateResponse.data.answer) {
           const parsedData = parseResponse(updateResponse.data.answer);
-          // dispatch(
-          //   updatePage({ pageId: currentPage?.pageId || 0, ...parsedData })
-          // );
           dispatch(
             addPage({ ...parsedData }) // 새로운 페이지를 추가
           );
 
-          // 두번째 API 요청 : 이야기 저장
+          // 다음 API 요청 : 이야기 저장
           const saveStoryResponse = await axios.post(
             "http://localhost:8000/api/story/save_story/",
             { story_id: story, content: parsedData.content },
@@ -244,36 +193,49 @@ const FirstResultPage: React.FC = () => {
           );
           console.log("저장된 이야기 내용 :", saveStoryResponse);
 
-          // 이미지 생성 요청
-          const imageResponse = await axios.post(
-            "http://localhost:8000/api/story/register/chatgpt/image/",
-            { story_id: story, query: parsedData.content },
-            config
-          );
+          await createAndSaveImage(parsedData.pageId, parsedData.content);
 
-          if (imageResponse.status === 200 && imageResponse.data.image_url) {
-            console.log("이미지 생성 성공:", imageResponse.data.image_url);
+          // // 이미지 생성 요청
+          // const imageResponse = await axios.post(
+          //   "http://localhost:8000/api/story/register/chatgpt/image/",
+          //   { story_id: story, query: parsedData.content },
+          //   config
+          // );
 
-            // 이미지 저장 요청
-            await axios.post(
-              "http://localhost:8000/api/story/save_image/",
-              {
-                story_id: story,
-                page_number: currentPage?.pageId,
-                image_url: imageResponse.data.image_url,
-              },
-              config
-            );
-            console.log("이미지 저장 성공");
-            dispatch(
-              setImage({
-                pageId: currentPage?.pageId,
-                imageUrl: imageResponse.data.image_url,
-              })
-            );
-          } else {
-            console.error("이미지 생성 요청 실패:", imageResponse);
-          }
+          // if (imageResponse.status === 200 && imageResponse.data.image_url) {
+          //   console.log("이미지 생성 성공:", imageResponse.data.image_url);
+
+          //   // 이미지 저장 요청
+          //   await axios.post(
+          //     "http://localhost:8000/api/story/save_image/",
+          //     {
+          //       story_id: story,
+          //       page_number: currentPage?.pageId,
+          //       image_url: imageResponse.data.image_url,
+          //     },
+          //     config
+          //   );
+          //   console.log("이미지 저장 성공");
+          //   dispatch(
+          //     setImage({
+          //       pageId: currentPage?.pageId,
+          //       imageUrl: imageResponse.data.image_url,
+          //     })
+          //   );
+          //         } else {
+          //           console.error("이미지 생성 요청 실패:", updateResponse);
+          //         }
+          //       } else {
+          //         console.error("이야기 저장 요청 실패 :", updateResponse);
+          //       }
+          //     } catch (error) {
+          //       console.error("API 요청 중 오류가 발생했습니다:", error);
+          //     } finally {
+          //       setLoading(false);
+          //     }
+          //   },
+          //   [dispatch, story, currentPage, config]
+          // );
         } else {
           console.error("이야기 저장 요청 실패 :", updateResponse);
         }
@@ -283,7 +245,7 @@ const FirstResultPage: React.FC = () => {
         setLoading(false);
       }
     },
-    [dispatch, story, currentPage, fetchConfig]
+    [dispatch, story, currentPage, config]
   );
 
   return (
@@ -317,12 +279,6 @@ const FirstResultPage: React.FC = () => {
           className="flex-1 p-8 w-full items-center justify-center bg-[#FDF9F6] text-lg"
           style={{ height: "700px" }}
         >
-          {/* {currentPage?.imageUrl ? (
-            <img src={currentPage.imageUrl} alt="Story Illustration" />
-          ) : (
-            <p>이미지 로딩 중...</p>
-          )} */}
-
           {currentPage?.imageUrl ? (
             loading ? (
               <LoadingSpinner />
@@ -332,6 +288,10 @@ const FirstResultPage: React.FC = () => {
                   src={currentPage.imageUrl}
                   alt="Story Illustration"
                   onLoad={() => setLoading(false)}
+                  onError={() => {
+                    console.error("이미지 로드 실패:", currentPage.imageUrl);
+                    setLoading(false);
+                  }}
                 />
               </div>
             )
@@ -343,7 +303,7 @@ const FirstResultPage: React.FC = () => {
 
       <div className="flex w-full items-center justify-center bg-[#FDF9F6] border-t border-gray-300 p-3">
         {/* 임시 방편 */}
-        <p>{pages.length - 1}</p>
+        <p>{pages.length}</p>
       </div>
       <div className="flex items-center justify-center">
         {pages.length >= 5 && (
